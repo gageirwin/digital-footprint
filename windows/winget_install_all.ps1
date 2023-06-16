ca2dcd7e9f748e320f86f61de7d8e02fd9cc0f06
Gage Irwin <91108814+gageirwin@users.noreply.github.com>
2023-06-15 20:23:09 -0500

    Create winget_install_all.ps1

---
+++ windows/winget_install_all.ps1
@@ -0,0 +1,33 @@
+winget install -e --id dotPDNLLC.paintdotnet `
+    install -e --id Valve.Steam `
+    install -e --id VSCodium.VSCodium `
+    install -e --id OBSProject.OBSStudio `
+    install -e --id Brave.Brave `
+    install -e --id Mozilla.Firefox `
+    install -e --id Google.Chrome `
+    install -e --id AppWork.JDownloader `
+    install -e --id OpenWhisperSystems.Signal `
+    install -e --id Python.Python.3.11 `
+    install -e --id AutoHotkey.AutoHotkey `
+    install -e --id Yubico.Authenticator -v 5.1.0 `
+    install -e --id PrivateInternetAccess.PrivateInternetAccess `
+    install -e --id GitHub.GitHubDesktop `
+    install -e --id Notepad++.Notepad++ `
+    install -e --id BlueStack.BlueStacks `
+    install -e --id CPUID.HWMonitor `
+    install -e --id WireGuard.WireGuard `
+    install -e --id Microsoft.PowerToys `
+    install -e --id OO-Software.ShutUp10 `
+    install -e --id Docker.DockerDesktop `
+    install -e --id Gyan.FFmpeg `
+    install -e --id Git.Git `
+    install -e --id TechPowerUp.NVCleanstall `
+    install -e --id Bitwarden.Bitwarden `
+    install -e --id CPUID.CPU-Z `
+    install -e --id TechPowerUp.GPU-Z `
+    install -e --id CrystalDewWorld.CrystalDiskInfo `
+    install -e --id CrystalDewWorld.CrystalDiskMark `
+    install -e --id Nextcloud.NextcloudDesktop `
+    install -e --id Oracle.VirtualBox `
+    install -e --id VideoLAN.VLC `
+    install -e --id qBittorrent.qBittorrent
